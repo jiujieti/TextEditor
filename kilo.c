d243e00c46eadfda40e1c6f1ac3bcb215a71a2a5
jiujieti <melody2014ymq@gmail.com>
2017-05-21 13:43:46 +0200

    add comments

--- kilo.c
+++ kilo.c
@@ -1,20 +1,31 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-// read original terminal ios
+/*** data ***/
+
+/* read original terminal ios */
 struct termios orig_termios;
 
-// disable raw mode
-void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+/*** terminal ***/
+
+/* error handling */
+void die(const char *s) {
+  perror(s);
+  exit(1);
 }
 
-// enable raw mode
+/* disable raw mode */
+void disableRawMode() {
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+/* enable raw mode */
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 
   // disable raw mode when the program exits, either by main or exit()  
   atexit(disableRawMode);
@@ -28,15 +39,17 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
   enableRawMode();
 
   while(1) {
     char c;
-    read(STDIN_FILENO, &c, 1);
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if(iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
